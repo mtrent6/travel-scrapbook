b09f9adb704c03914ce9bf33659071ef3e9cc80f
Trenton Morrell <trentkm@amazon.com>
2022-08-01 12:10:46 -0400

    adding blur css

--- croatia-scrapbook/src/routes/krka.jsx
+++ croatia-scrapbook/src/routes/krka.jsx
@@ -3,6 +3,8 @@ import Falls from "../images/Krka/falls.jpg"
 import Water from "../images/Krka/water.jpg"
 import SplitBlur from "../images/Krka/split_blur.png"
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 
 export default function Krka() {
     const s = <img style={{ width: "100%", height: "100%" }} src={Split} alt="island" />
@@ -10,7 +12,7 @@ export default function Krka() {
     return (
         <>
             <div style={{ width: "100%", height: 500 }}>
-                <LazyLoadImage alt="island" effect="blur" height={500} width={"100%"} src={Split} />
+                <LazyLoadImage delayTime={300} alt="island" effect="blur" height={500} width={"100%"} src={Split} />
 
             </div>
             <div style={{ width: "100%", height: 500 }}>
