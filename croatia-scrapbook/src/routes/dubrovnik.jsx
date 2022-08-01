942a94b8d3226edb039d73b13094cd0b0eb7f8a8
Trenton Morrell <trentkm@amazon.com>
2022-07-31 21:17:26 -0400

    adding more photos

--- croatia-scrapbook/src/routes/dubrovnik.jsx
+++ croatia-scrapbook/src/routes/dubrovnik.jsx
@@ -1,11 +1,20 @@
 import DubrovnikIsland from "../images/Dubrovnik/DubrovnikIsland.jpg"
+import Roof from "../images/Dubrovnik/roof.jpeg"
+import Rocks from "../images/Dubrovnik/rocks.jpeg"
+
 export default function Dubrovnik() {
     return (
         <>
-            <h2>Dubrovnik</h2>
             <div style={{ width: "100%", height: 300 }}>
                 <img style={{ width: "100%", height: "100%" }} src={DubrovnikIsland} alt="island" />
             </div>
+            <div style={{ width: "100%", height: 300 }}>
+                <img style={{ width: "100%", height: "100%" }} src={Roof} alt="island" />
+            </div>
+            <div style={{ width: "100%", height: 400 }}>
+                <img style={{ width: "100%", height: "100%" }} src={Rocks} alt="island" />
+            </div>
+
 
         </>
     );
