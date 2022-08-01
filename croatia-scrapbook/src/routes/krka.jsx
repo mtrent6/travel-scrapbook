f74e72423e5254588d920150fae05107b370fdde
Trenton Morrell <trentkm@amazon.com>
2022-08-01 11:59:24 -0400

    testing lazy loading

--- croatia-scrapbook/src/routes/krka.jsx
+++ croatia-scrapbook/src/routes/krka.jsx
@@ -1,18 +1,23 @@
 import Split from "../images/Krka/split.jpg"
 import Falls from "../images/Krka/falls.jpg"
 import Water from "../images/Krka/water.jpg"
+import SplitBlur from "../images/Krka/split_blur.png"
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export default function Krka() {
+    const s = <img style={{ width: "100%", height: "100%" }} src={Split} alt="island" />
+
     return (
         <>
             <div style={{ width: "100%", height: 500 }}>
-                <img style={{ width: "100%", height: "100%" }} src={Split} alt="island" />
+                <LazyLoadImage alt="island" effect="blur" height={500} width={"100%"} src={Split} />
+
             </div>
             <div style={{ width: "100%", height: 500 }}>
-                <img style={{ width: "100%", height: "100%" }} src={Falls} alt="island" />
+                <LazyLoadImage alt="island" effect="blur" height={500} width={"100%"} src={Falls} />
             </div>
             <div style={{ width: "100%", height: 500 }}>
-                <img style={{ width: "100%", height: "100%" }} src={Water} alt="island" />
+                <LazyLoadImage alt="island" effect="blur" height={500} width={"100%"} src={Water} />
             </div>
         </>
     );
